2a53b5cec01e942b359caf8b4096fc6a5615178a
onelegchair <onelegchair@gmail.com>
2021-01-20 16:59:51 -0600

    Added check for which screen the user is on. Set main screen as function. Changed creating title and button labels to functions.

--- code.py
+++ code.py
@@ -29,27 +29,30 @@ dirty_screen = 1
 settings_screen = 2
 cleaning_screen = 3
 clean_screen = 4
-
-# Add Title
-magtag.graphics.set_background(dishes_image)
-magtag.add_text(
-    text_font=terminalio.FONT,
-    text_position=(
-        5,
-        (magtag.graphics.display.height // 2) - 1,
-    ),
-    text_scale=2,
-)
-magtag.set_text("Dish Bish")
+index_count = 0
 current_screen = start_screen
 
-# Add Button Labels
-magtag.add_text(
-    text_font=terminalio.FONT,
-    text_position=(3, 120),
-    text_scale=1,
-)
-magtag.set_text(" Dirty      Settings                  Clean", 1)
+# Define Functions
+def set_button_label(label, pos):
+    global index_count
+    magtag.add_text(
+        text_font = terminalio.FONT,
+        text_position = (pos, 120),
+        text_scale = 1,
+        )
+    magtag.set_text(label, index_count, False)
+    index_count += 1
+
+def set_title(label, pos, scale):
+    global index_count
+    magtag.graphics.set_background(dishes_image)
+    magtag.add_text(
+        text_font = terminalio.FONT,
+        text_position = (pos, (magtag.graphics.display.height // 2) - 1),
+        text_scale = scale,
+    )
+    magtag.set_text(label, index_count, False)
+    index_count += 1
 
 
 def blink(color, count):
@@ -68,9 +71,24 @@ def blink(color, count):
         magtag.peripherals.neopixel_disable = True
         time.sleep(0.5)
 
+def main():
+    global index_count
+    # Add Title
+    set_title('Dish Bish', 5, 3)
+
+    # Add Button Labels
+    button_label_a = set_button_label('Dirty', 5)
+    button_label_b = set_button_label('Settings', 75)
+
+    magtag.refresh()
+
+
+main()
+
 while True:
-    if magtag.peripherals.button_a_pressed:
+    if magtag.peripherals.button_a_pressed and current_screen != settings_screen:
         magtag.graphics.set_background(dirty_dishes_image)
+        set_title()
         magtag.add_text(
             text_font=terminalio.FONT,
             text_position=(
@@ -84,16 +102,18 @@ while True:
         blink(RED, 2)
         current_screen = dirty_screen
 
-    if magtag.peripherals.button_b_pressed:
+    if magtag.peripherals.button_a_pressed and current_screen == settings_screen:
+        main()
+
+    if magtag.peripherals.button_b_pressed and current_screen != settings_screen:
+        current_screen = settings_screen
         magtag.set_background(WHITE)
         magtag.set_text("")
-        current_screen = settings_screen
 
-
-    if magtag.peripherals.button_c_pressed:
+    if magtag.peripherals.button_c_pressed and current_screen != settings_screen:
         pass
 
-    if magtag.peripherals.button_d_pressed:
+    if magtag.peripherals.button_d_pressed and current_screen != settings_screen:
         magtag.graphics.set_background(clean_dishes_image)
         magtag.add_text(
             text_font=terminalio.FONT,
